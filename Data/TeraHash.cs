a30a7876f74abb924b7ec3f08506af2ec9fd8d88
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2023-10-29 14:29:04 +0800

    添加项目文件。

---
+++ Data/TeraHash.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeraIO.Data
+{
+    public class TeraHash
+    {
+        public TeraHash(byte[] bytes)
+        {
+
+        }
+
+        public static List<bool> ByteArrayToBoolList(byte[] byteArray)
+        {
+            List<bool> boolList = new List<bool>();
+
+            foreach (byte b in byteArray)
+            {
+                boolList.Add((b & 1) != 0);
+            }
+
+            return boolList;
+        }
+
+        public static List<bool[,]> ChunkBools(List<bool> bools)
+        {
+            List<bool[,]> result = new List<bool[,]>();
+            int count = 0;
+            result.Add(new bool[8, 8]);
+            foreach (bool i in bools)
+            {
+                if (count >= 64)
+                {
+                    result.Add(new bool[8, 8]);
+                    count = 0;
+                }
+                result.Last()[count % 8, count / 8] = i;
+                count++;
+            }
+            return result;
+        }
+    }
+}
---
+++ TeraIO.Test/TeraIO.Test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using TeraIO;
+using TeraIO.Data;
+
+namespace TeraIO.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = File.ReadAllBytes("P:/NikGapps-tiny-arm64-11-20220324-signed.zip");
+            List<bool> bools = TeraHash.ByteArrayToBoolList(data);
+            HammingCode hammingCode = new HammingCode(bools);
+            string i = ToString(hammingCode.hammingCode);
+            Console.WriteLine(i);
+            Console.WriteLine(hammingCode);
+        }
+
+        public static string ToString(List<bool[,]> args)
+        {
+            string result = "";
+            foreach (bool[,] i in args)
+            {
+                foreach (bool j in i)
+                {
+                    result += j ? "1" : "0";
+                }
+                result += " ";
+            }
+            return result;
+        }
+
+        public static string ToString(List<bool> args)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < args.Count; i++)
+            {
+                sb.Append(args[i] ? 1 : 0);
+            }
+            return sb.ToString();
+        }
+    }
+}
